cfdb8fc52c4c2c3f77d55003c293573f64124702
HumJ <tlos142857@live.cn>
2022-03-15 11:02:47 +0800

    use ImageSharp

---
+++ src/devices/MemoryLCD/LSxxxB7DHxxImageSending.cs
@@ -0,0 +1,91 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Iot.Device.MemoryLcd
+{
+    /// <summary>
+    /// Extend methods for LSxxxB7DHxx
+    /// </summary>
+    public static class LSxxxB7DHxxImageSending
+    {
+        /// <summary>
+        /// Method for whether pixel is lit or not.
+        /// </summary>
+        /// <param name="pixel">Input pixel</param>
+        /// <returns>True if lit.</returns>
+        public delegate bool BrightnessConvertor(IPixel pixel);
+
+        /// <summary>
+        /// Show image to device
+        /// </summary>
+        /// <param name="mlcd">Memory LCD device</param>
+        /// <param name="image">Image to show</param>
+        /// <param name="brightnessConvertor">Method for whether pixel is lit or not. Use <see cref="LinearBrightnessConvertor"/> if null.</param>
+        /// <param name="split">Number to splits<br/>To avoid buffer overflow exceptions, it needs to split one frame into multiple sends for some device.</param>
+        /// <param name="frameInversion">Frame inversion flag</param>
+        public static void ShowImage<TPixel>(this LSxxxB7DHxx mlcd, Image<TPixel> image, BrightnessConvertor? brightnessConvertor = null, int split = 1, bool frameInversion = false)
+                where TPixel : unmanaged, IPixel<TPixel>
+        {
+            mlcd.FillImageBufferWithImage(image, brightnessConvertor ?? LinearBrightnessConvertor);
+
+            int linesToSend = mlcd.PixelHeight / split;
+            int bytesToSend = mlcd._frameBuffer.Length / split;
+
+            for (int fs = 0; fs < split; fs++)
+            {
+                Span<byte> lineNumbers = mlcd._lineNumberBuffer.AsSpan(linesToSend * fs, linesToSend);
+                Span<byte> bytes = mlcd._frameBuffer.AsSpan(bytesToSend * fs, bytesToSend);
+
+                mlcd.DataUpdateMultipleLines(lineNumbers, bytes, frameInversion);
+            }
+        }
+
+        /// <summary>
+        /// Lit if average value of RGB is greater than half.
+        /// </summary>
+        /// <param name="pixel">Input pixel</param>
+        /// <returns>True if average value of RGB is greater than half.</returns>
+        public static bool LinearBrightnessConvertor(IPixel pixel)
+        {
+            var vector = pixel.ToScaledVector4();
+            return vector.X + vector.Y + vector.Z > 1.5;
+        }
+
+        private static void FillImageBufferWithImage<TPixel>(this LSxxxB7DHxx mlcd, Image<TPixel> image, BrightnessConvertor brightnessConvertor)
+            where TPixel : unmanaged, IPixel<TPixel>
+        {
+            if (image.Width != mlcd.PixelWidth)
+            {
+                throw new ArgumentException($"The width of the image should be {mlcd.PixelWidth}", nameof(image));
+            }
+
+            if (image.Height != mlcd.PixelHeight)
+            {
+                throw new ArgumentException($"The height of the image should be {mlcd.PixelHeight}", nameof(image));
+            }
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x += 8)
+                {
+                    int bx = x / 8;
+                    byte dataByte = (byte)(
+                        (brightnessConvertor(image[x + 0, y]) ? 0b10000000 : 0) |
+                        (brightnessConvertor(image[x + 1, y]) ? 0b01000000 : 0) |
+                        (brightnessConvertor(image[x + 2, y]) ? 0b00100000 : 0) |
+                        (brightnessConvertor(image[x + 3, y]) ? 0b00010000 : 0) |
+                        (brightnessConvertor(image[x + 4, y]) ? 0b00001000 : 0) |
+                        (brightnessConvertor(image[x + 5, y]) ? 0b00000100 : 0) |
+                        (brightnessConvertor(image[x + 6, y]) ? 0b00000010 : 0) |
+                        (brightnessConvertor(image[x + 7, y]) ? 0b00000001 : 0));
+
+                    mlcd._frameBuffer[bx + y * mlcd.BytesPerLine] = dataByte;
+                }
+            }
+        }
+    }
+}
--- src/devices/MemoryLCD/samples/Program.cs
+++ src/devices/MemoryLCD/samples/Program.cs
@@ -4,130 +4,68 @@
 using System;
 using System.Device.Gpio;
 using System.Device.Spi;
-using System.Drawing;
-using System.Linq;
-using System.Threading.Tasks;
 using Iot.Device.MemoryLcd;
-using MemoryLcd.Extends;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
-namespace MemoryLcd.Samples
+// *** LSxxxB7DHxx's chip select is high-level votage enabled ***
+// You can use default ChipSelectLineActiveState value and a NOT gate to inverse this signal
+// If you use gpio pins to control SCS, ChipSelectLineActiveState is optional
+SpiDevice spi = SpiDevice.Create(new SpiConnectionSettings(0, 0)
 {
-    internal class Program
+    ChipSelectLineActiveState = 1, // optional
+    ClockFrequency = 2_000_000,
+    DataFlow = DataFlow.MsbFirst,
+    Mode = 0
+});
+
+// You can fix DISP and EXTCOMIN in your circuit and use SPI's CE line to economize the GPIO pins
+// DISP -- HIGH
+// EXTCOMIN -- LOW
+// EXTMODE -- LOW
+// LSxxxB7DHxx mlcd = new LS027B7DH01(spi);
+GpioController gpio = new GpioController(PinNumberingScheme.Logical);
+LSxxxB7DHxx mlcd = new LS027B7DH01(spi, gpio, false, 25, 24, 23);
+
+Clear();
+Console.ReadLine();
+
+RandomDots();
+Console.ReadLine();
+
+ShowImage();
+
+void Clear()
+{
+    Console.WriteLine();
+    Console.WriteLine("Clear");
+
+    var image = new Image<Rgb24>(mlcd.PixelWidth, mlcd.PixelHeight);
+    mlcd.ShowImage(image, split: 4);
+}
+
+void RandomDots()
+{
+    Console.WriteLine("Random dots");
+
+    var image = new Image<Rgb24>(mlcd.PixelWidth, mlcd.PixelHeight);
+
+    var random = new Random();
+    for (var x = 0; x < image.Width; x++)
     {
-        private static void Main(string[] args)
+        for (var y = 0; y < image.Height; y++)
         {
-            // *** LSxxxB7DHxx's chip select is high-level votage enabled ***
-            // You can use default ChipSelectLineActiveState value and a NOT gate to inverse this signal
-            // If you use gpio pins to control SCS, ChipSelectLineActiveState is optional
-            SpiDevice spi = SpiDevice.Create(new SpiConnectionSettings(0, 0)
-            {
-                ChipSelectLineActiveState = 1, // optional
-                ClockFrequency = 2_000_000,
-                DataFlow = DataFlow.MsbFirst,
-                Mode = 0
-            });
-
-            // You can fix DISP and EXTCOMIN in your circuit and use SPI's CE line to economize the GPIO pins
-            // DISP -- HIGH
-            // EXTCOMIN -- LOW
-            // EXTMODE -- LOW
-            // LSxxxB7DHxx mlcd = new LS027B7DH01(spi);
-            GpioController gpio = new GpioController(PinNumberingScheme.Logical);
-            LSxxxB7DHxx mlcd = new LS027B7DH01(spi, gpio, false, 25, 24, 23);
-
-            Random random = new Random();
-
-            Console.WriteLine("Clear. Press any key to continue...");
-            Console.ReadKey();
-
-            mlcd.AllClear();
-
-            Console.WriteLine("DataUpdate1Line. Press any key to continue...");
-            Console.ReadKey();
-
-            byte[] lineBuffer = new byte[mlcd.BytesPerLine];
-            for (int i = 0; i < mlcd.PixelHeight; i++)
-            {
-                random.NextBytes(lineBuffer);
-                mlcd.DataUpdate1Line((byte)i, lineBuffer);
-            }
-
-            Console.WriteLine("Clear(inverse). Press any key to continue...");
-            Console.ReadKey();
-
-            mlcd.AllClear(true);
-
-            Console.WriteLine("DataUpdateMultipleLines. Press any key to continue...");
-            Console.ReadKey();
-
-            byte[] lineNumberBuffer = Enumerable.Range(0, mlcd.PixelHeight).Select(m => (byte)m).ToArray();
-            byte[] frameBuffer = new byte[mlcd.BytesPerLine * mlcd.PixelHeight];
-
-            // LS027B7DH01 needs split bytes into 4 spans on RaspberryPi
-            int frameSplit = 4; // mlcd is LS027B7DH01 ? 4 : 1;
-            int linesToSend = mlcd.PixelHeight / frameSplit;
-            int bytesToSend = frameBuffer.Length / frameSplit;
-
-            random.NextBytes(frameBuffer);
-            for (int fs = 0; fs < frameSplit; fs++)
-            {
-                Span<byte> lineNumbers = lineNumberBuffer.AsSpan(linesToSend * fs, linesToSend);
-                Span<byte> bytes = frameBuffer.AsSpan(bytesToSend * fs, bytesToSend);
-
-                mlcd.DataUpdateMultipleLines(lineNumbers, bytes);
-            }
-
-            Console.WriteLine("Show image. Press any key to continue...");
-            Console.ReadKey();
-
-            Bitmap image = new Bitmap(mlcd.PixelWidth, mlcd.PixelHeight);
-            Graphics graphics = Graphics.FromImage(image);
-            graphics.Clear(Color.White);
-
-            graphics.DrawImage(Image.FromFile("./images/dotnet-bot.bmp"), 4, 4);
-            graphics.DrawImage(Image.FromFile("./images/github-dotnet-iot-black.bmp"), 136, 4);
-            graphics.DrawImage(Image.FromFile("./images/shapes.bmp"), 268, 4);
-
-            Brush clearScreenBrush = new SolidBrush(Color.White);
-            Brush textBrush = new SolidBrush(Color.Black);
-            Font textFont = new Font("Sans Serif", 12, FontStyle.Regular);
-
-            int fpsCpunter = 0;
-            int fps = 0;
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    fps = fpsCpunter;
-                    fpsCpunter = 0;
-                    Task.Delay(1000 - DateTime.Now.Millisecond).Wait();
-                }
-            });
-
-            while (true)
-            {
-                graphics.FillRectangle(clearScreenBrush, 4, 40, 392, 196);
-
-                DateTime time = DateTime.Now;
-                string message = string.Join(Environment.NewLine, new[]
-                {
-                    $"Memory LCD model: {mlcd.GetType().Name}",
-                    string.Empty,
-                    $"Pixels: {mlcd.PixelWidth} x {mlcd.PixelHeight}",
-                    string.Empty,
-                    $"Current time: {time}",
-                    string.Empty,
-                    $"Current time ticks: {time.Ticks}",
-                    string.Empty,
-                    $"FPS: {fps}",
-                });
-                graphics.DrawString(message, textFont, textBrush, 4, 40);
-
-                mlcd.ShowImage(image, 4);
-
-                fpsCpunter++;
-                Task.Delay(0).Wait();
-            }
+            image[x, y] = new Rgb24((byte)random.Next(), (byte)random.Next(), (byte)random.Next());
         }
     }
+
+    mlcd.ShowImage(image, split: 4);
+}
+
+void ShowImage()
+{
+    Console.WriteLine("Show image");
+
+    var image = Image.Load<Rgb24>("./dotnet-bot.png");
+    mlcd.ShowImage(image, split: 4);
 }
